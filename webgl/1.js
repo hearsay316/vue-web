97384e1cdc5b191b272dfb7de503dfb05c61160f
hearsay316 <hearsay316@gmail.com>
2019-10-11 17:17:47 +0800

    开始学习webgl

---
+++ webgl/1.js
@@ -0,0 +1,17 @@
+/*class tree {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    console.log(this.name);
+    return this.name;
+  }
+}
+var name = "this";
+let obj = new tree("尼古拉斯赵四");
+let fn = obj.getName.bind(window);
+fn();*/
+console.log(null == undefined);
+console.log(null - 0 == 0);
+console.log(undefined - 0);
+Object.defineProperty();
